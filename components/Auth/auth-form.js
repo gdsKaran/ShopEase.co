e25b6089e81ddaa0af2529cb1ad33c3f325f31ae
gdsKaran <gagan.null@gmail.com>
2025-02-22 02:13:01 +0530

    w

--- components/Auth/auth-form.js
+++ components/Auth/auth-form.js
@@ -11,129 +11,127 @@ export default function AuthForm({ mode }) {
   );
   return (
     <>
-      <div className="min-h-screen">
-        <div className="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-          <div className="w-full max-w-sm space-y-10">
+      <div className="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+        <div className="w-full max-w-sm space-y-10">
+          <div>
+            <Image
+              width={0}
+              height={0}
+              sizes="(max-width: 768px) 100vw, 33vw"
+              alt="Sign Up"
+              src="https://cdn3.iconfinder.com/data/icons/simple-toolbar/512/lock_secure_security_password_private_key-512.png"
+              className="mx-auto h-10 w-auto"
+            />
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+              {mode === "signin" ? "Sign in to" : "Create"} your account
+            </h2>
+          </div>
+
+          <form action={formAction} className="space-y-6">
+            {formState.errors && (
+              <ul id="form-errors">
+                {Object.keys(formState.errors).map((error) => (
+                  <AuthAlert key={error} alert={formState.errors[error]} />
+                ))}
+              </ul>
+            )}
             <div>
-              <Image
-                width={0}
-                height={0}
-                sizes="(max-width: 768px) 100vw, 33vw"
-                alt="Sign Up"
-                src="https://cdn3.iconfinder.com/data/icons/simple-toolbar/512/lock_secure_security_password_private_key-512.png"
-                className="mx-auto h-10 w-auto"
-              />
-              <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                {mode === "signin" ? "Sign in to" : "Create"} your account
-              </h2>
+              <div className="col-span-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="Email address"
+                  autoComplete="email"
+                  aria-label="Email address"
+                  className="block w-full rounded-t-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:relative focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+              <div className="-mt-px">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  placeholder="Password"
+                  autoComplete="current-password"
+                  aria-label="Password"
+                  className="block w-full rounded-b-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:relative focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
             </div>
 
-            <form action={formAction} className="space-y-6">
-              {formState.errors && (
-                <ul id="form-errors">
-                  {Object.keys(formState.errors).map((error) => (
-                    <AuthAlert key={error} alert={formState.errors[error]} />
-                  ))}
-                </ul>
-              )}
-              <div>
-                <div className="col-span-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    placeholder="Email address"
-                    autoComplete="email"
-                    aria-label="Email address"
-                    className="block w-full rounded-t-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:relative focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-                <div className="-mt-px">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    placeholder="Password"
-                    autoComplete="current-password"
-                    aria-label="Password"
-                    className="block w-full rounded-b-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:relative focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="flex gap-3">
-                  <div className="flex h-6 shrink-0 items-center">
-                    <div className="group grid size-4 grid-cols-1">
-                      <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="col-start-1 row-start-1 appearance-none rounded border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+            <div className="flex items-center justify-between">
+              <div className="flex gap-3">
+                <div className="flex h-6 shrink-0 items-center">
+                  <div className="group grid size-4 grid-cols-1">
+                    <input
+                      id="remember-me"
+                      name="remember-me"
+                      type="checkbox"
+                      className="col-start-1 row-start-1 appearance-none rounded border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+                    />
+                    <svg
+                      fill="none"
+                      viewBox="0 0 14 14"
+                      className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-[:disabled]:stroke-gray-950/25"
+                    >
+                      <path
+                        d="M3 8L6 11L11 3.5"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="opacity-0 group-has-[:checked]:opacity-100"
                       />
-                      <svg
-                        fill="none"
-                        viewBox="0 0 14 14"
-                        className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-[:disabled]:stroke-gray-950/25"
-                      >
-                        <path
-                          d="M3 8L6 11L11 3.5"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          className="opacity-0 group-has-[:checked]:opacity-100"
-                        />
-                        <path
-                          d="M3 7H11"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          className="opacity-0 group-has-[:indeterminate]:opacity-100"
-                        />
-                      </svg>
-                    </div>
+                      <path
+                        d="M3 7H11"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="opacity-0 group-has-[:indeterminate]:opacity-100"
+                      />
+                    </svg>
                   </div>
-                  <label
-                    htmlFor="remember-me"
-                    className="block text-sm/6 text-gray-900"
-                  >
-                    Remember me
-                  </label>
                 </div>
-                {mode === "signin" && (
-                  <div className="text-sm/6">
-                    <a
-                      href="/authentication/?mode=register"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      register?
-                    </a>
-                  </div>
-                )}
-                {mode === "register" && (
-                  <div className="text-sm/6">
-                    <a
-                      href="/authentication/?mode=signin"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Login with an existing account?
-                    </a>
-                  </div>
-                )}
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                <label
+                  htmlFor="remember-me"
+                  className="block text-sm/6 text-gray-900"
                 >
-                  {mode === "signin" ? "Sign in" : "Create Account"}
-                </button>
+                  Remember me
+                </label>
               </div>
-            </form>
-          </div>
+              {mode === "signin" && (
+                <div className="text-sm/6">
+                  <a
+                    href="/authentication/?mode=register"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    register?
+                  </a>
+                </div>
+              )}
+              {mode === "register" && (
+                <div className="text-sm/6">
+                  <a
+                    href="/authentication/?mode=signin"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Login with an existing account?
+                  </a>
+                </div>
+              )}
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                {mode === "signin" ? "Sign in" : "Create Account"}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
